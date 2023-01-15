6e1222068f257f3a52633e6afadd95009e7719ee
AntonAcorn <ShustikovAnimation@gmail.com>
2023-01-15 18:37:55 +0300

    Created a controller for distributing messages to queues in the message broker Created services for integrating with RabbitMQ Fixed several bugs discovered in the previous lesson.

---
+++ dispatcher/src/main/java/ru/acorn/service/impl/ProduceToRabbitMqImpl.java
@@ -0,0 +1,23 @@
+package ru.acorn.service.impl;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.acorn.service.ProduceToRabbitMq;
+
+@Service
+@Log4j
+public class ProduceToRabbitMqImpl implements ProduceToRabbitMq {
+    private final RabbitTemplate rabbitTemplate;
+
+    public ProduceToRabbitMqImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+//        rabbitTemplate.convertAndSend(rabbitQueue, update);
+    }
+}
