f059200d09d0da668a5a86a3b388ed01c2a0897f
AntonAcorn <ShustikovAnimation@gmail.com>
2023-01-31 17:40:29 +0300

    Bug fixing (get-doc method)

--- rest-service/src/main/java/ru/acorn/controller/FileController.java
+++ rest-service/src/main/java/ru/acorn/controller/FileController.java
@@ -7,6 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.acorn.service.FileService;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/file")
 @Log4j
@@ -18,26 +21,27 @@ public class FileController {
     }
 
     @GetMapping("/get-doc")
-    public ResponseEntity<?> getDoc (@RequestParam (name = "id") String id) {
+    public void getDoc (@RequestParam (name = "id") String id, HttpServletResponse response) {
         var doc = fileService.findDocById(id);
         if(doc == null) {
             log.error("Doc is not found");
-            return ResponseEntity.badRequest().body("Doc is not found");
+            return;
         }
+        response.setContentType(MediaType.parseMediaType(doc.getMimeType()).toString());
+        response.setHeader("Content-disposition", "attachment; filename=" + doc.getFilename());
+        response.setStatus(HttpServletResponse.SC_OK);
 
         var binaryContent = doc.getBinaryContent();
-        FileSystemResource fileSystemResource = fileService.getFileSystemResource(binaryContent);
-
-        if(!(fileSystemResource.isFile())){
-            log.error("Server problem");
-            return ResponseEntity.internalServerError().build();
+        try{
+            var out = response.getOutputStream();
+            out.write(binaryContent.getFileArraysOfBytes());
+            out.close();
+        } catch (IOException e) {
+            log.error(e);
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(doc.getMimeType()))
-                .header("Content-disposition", "attachment; filename=" + doc.getFilename())
-                .body(fileSystemResource);
     }
+
     @GetMapping("/get-photo")
     public ResponseEntity<?> getPhoto (@RequestParam(name = "id") String id){
         var photo = fileService.findPhotoById(id);
