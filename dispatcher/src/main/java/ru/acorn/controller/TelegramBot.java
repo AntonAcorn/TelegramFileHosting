73df378fb645b2fb496f703688cc11eeefd08e20
AntonAcorn <ShustikovAnimation@gmail.com>
2023-01-31 18:09:23 +0300

    Refactoring TelegramBotClass for TelegramWebHook

--- dispatcher/src/main/java/ru/acorn/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/acorn/controller/TelegramBot.java
@@ -4,7 +4,10 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -12,11 +15,13 @@ import javax.annotation.PostConstruct;
 
 @Component
 @Log4j
-public class TelegramBot extends TelegramLongPollingBot {
+public class TelegramBot extends TelegramWebhookBot {
     @Value("${bot.name}")
     private String botName;
     @Value("${bot.token}")
     private String botToken;
+    @Value("${bot.uri}")
+    private String botUri;
 
     private UpdateProcessor updateProcessor;
 
@@ -27,6 +32,14 @@ public class TelegramBot extends TelegramLongPollingBot {
     @PostConstruct
     public void registerUpdateController() {
         updateProcessor.registerBot(this);
+        try{
+            var setWebhook = SetWebhook.builder()
+                    .url(botUri)
+                    .build();
+            this.setWebhook(setWebhook);
+        } catch (TelegramApiException e) {
+            log.error(e);
+        }
     }
 
     @Override
@@ -39,11 +52,6 @@ public class TelegramBot extends TelegramLongPollingBot {
         return botToken;
     }
 
-    @Override
-    public void onUpdateReceived(Update update) {
-        updateProcessor.processUpdate(update);
-    }
-
     public void sendAnswerMessage(SendMessage message) {
         if (message != null) {
             try {
@@ -52,5 +60,15 @@ public class TelegramBot extends TelegramLongPollingBot {
                 log.error(e);
             }
         }
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
     }
 }
